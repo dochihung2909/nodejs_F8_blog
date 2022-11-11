7f0006bc52b50b802166d7e390798cbf2d1dbc2a
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-11-11 09:59:57 +0700

    Install prettier, lint-staged,husky

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,10 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express') 
+const router = express.Router() 
 
 
-const siteController = require('../app/controllers/SiteControllers');
+const siteController = require('../app/controllers/SiteControllers')
 
-router.use('/search',siteController.search);
-router.use('/',siteController.home);
+router.use('/search',siteController.search)
+router.use('/',siteController.home)
 
-module.exports = router;
+module.exports = router
