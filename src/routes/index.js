1ff88341bcf9e945ce5d8ab6eb579ff78a9998a5
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-11-11 10:01:04 +0700

    fix husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const siteRouter = require('./site')
 
 function route(app) { 
     
-                app.use('/news', newsRouter)  
-                
-                app.use('/',siteRouter)
+                        app.use('/news', newsRouter)  
+                        
+                        app.use('/',siteRouter)
 }
 
 module.exports = route
