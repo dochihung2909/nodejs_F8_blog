2ab2bd81acf8e4e57635fe34470ec6bab4f0a0b1
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-11-16 23:06:45 +0700

    Select all

--- src/app/controllers/CourseControllers.js
+++ src/app/controllers/CourseControllers.js
@@ -29,12 +29,11 @@ class CourseControllers {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = req.body
-        formData.image = `https://i.ytimg.com/vi/${formData.videoId}/hq720.jpg`
-        const course = new Course(formData)
+        req.body.image = `https://i.ytimg.com/vi/${req.body.videoId}/hq720.jpg`
+        const course = new Course(req.body)
         course
             .save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch((error) => {})
     }
 
@@ -47,6 +46,61 @@ class CourseControllers {
                 res.redirect('/me/stored/courses')
             })
             .catch(next)
+    }
+
+    // [DELETE] /courses/:id
+    destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => {
+                res.redirect('back')
+            })
+            .catch(next)
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => {
+                res.redirect('back')
+            })
+            .catch(next)
+    }
+
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => {
+                res.redirect('back')
+            })
+            .catch(next)
+    }
+
+    // [POST] /courses/handel-form-actions
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            // Delete course
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next)
+                break
+            // Restore form trash
+            case 'restore':
+                Course.restore({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next)
+                break
+            case 'deleteForce':
+                // Course.deleteOne({ _id : {$in : req.body.courseIds}})
+                //     .then(() => {
+                //         res.redirect('back')
+                //     })
+                //     .catch(next)
+                break
+            default:
+                res.json({ message: 'Action is  invalid!' })
+                break
+        }
     }
 }
 
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,12 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const courseController = require('../app/controllers/CourseControllers')
+const { route } = require('./news')
 
 // [GET] /courses/creates
 router.get('/create', courseController.create)
 router.post('/store', courseController.store)
+router.post('/handle-form-actions', courseController.handleFormActions)
 router.get('/:id/edit', courseController.edit)
+router.patch('/:id/restore', courseController.restore)
+router.delete('/:id/force', courseController.forceDestroy)
 router.put('/:id', courseController.update)
+router.delete('/:id', courseController.destroy)
 // [GET] /courses/:slug
 router.get('/:slug', courseController.show)
 
